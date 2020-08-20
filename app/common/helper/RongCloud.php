b58267db77d616de6370eae140c3cdbb8218c5ca
yangxiushan <501942821@qq.com>
2020-08-20 11:08:52 +0800

    融云功能封装

--- app/common/helper/RongCloud.php
+++ app/common/helper/RongCloud.php
@@ -6,16 +6,18 @@
  * Time: 14:53
  */
 
+use RongCloud\RongCloud;
+
 /**
  * 融云
  * Class RongCloud
  */
-class RongCloud
+class RongCloudApp
 {
     private static $appKey = null;
     private static $appSecret = null;
 
-    private static function initConfig()
+    private static function init()
     {
         if (self::$appKey == null || self::$appSecret == null) {
             $config = config("account.rong_cloud");
@@ -24,4 +26,40 @@ class RongCloud
         }
     }
 
+    /**
+     * 注册用户
+     * @param $rcUserId
+     * @param $userName
+     * @param $headImageUrl
+     * @return array
+     */
+    public static function register($rcUserId, $userName, $headImageUrl) {
+        self::init();
+        $RongSDK = new RongCloud(self::$appKey, self::$appSecret);
+        $user = [
+            'id' => $rcUserId, //用户id
+            'name' => $userName,//用户名称
+            'portrait' => $headImageUrl //用户头像
+        ];
+        $register = $RongSDK->getUser()->register($user);
+        return $register;
+    }
+
+    /**
+     * 修改用户信息
+     * @param $rcUserId
+     * @param $userName
+     * @param $headImageUrl
+     */
+    public static function updateUserInfo($rcUserId, $userName, $headImageUrl)
+    {
+        self::init();
+        $RongSDK = new RongCloud(self::$appKey, self::$appSecret);
+        $user = [
+            'id' => $rcUserId,//用户id
+            'name' => $userName,//用户名称
+            'portrait' => $headImageUrl //用户头像
+        ];
+        $RongSDK->getUser()->update($user);
+    }
 }

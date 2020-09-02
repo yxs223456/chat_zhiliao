c3e01cb61d3609bbefc9567377a4201a5dc2fe2c
Yanglichao <18201695833@163.com>
2020-09-02 18:59:00 +0800

    MOD:edit geotools

--- app/provider.php
+++ app/provider.php
@@ -6,4 +6,5 @@ use app\Request;
 return [
     'think\Request'          => Request::class,
     'think\exception\Handle' => ExceptionHandle::class,
+    'geotools' => \League\Geotools\Geotools::class
 ];

c7e641b6e63027e5cb71c5e83566d430c33bd814
yangxiushan <501942821@qq.com>
2020-10-20 16:43:27 +0800

    判断私聊接口结构修改

---
+++ app/v1/transformer/im/CheckSendMessage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangxiushan
+ * Date: 2020-08-27
+ * Time: 14:15
+ */
+
+namespace app\v1\transformer\banner;
+
+use app\common\transformer\TransformerAbstract;
+
+class Home extends TransformerAbstract
+{
+
+    public function __construct(array $params = null)
+    {
+        parent::__construct($params);
+    }
+
+    public function transformData(array $data): array
+    {
+        $returnData = [
+            "is_free" => (int) $data["is_free"],
+            "price" => (int) $data["price"],
+            "bonus" => (int) $data["bonus"],
+        ];
+
+        return $returnData;
+    }
+}

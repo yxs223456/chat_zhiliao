b350b0936ccb8f81d04e218d9e7b8234a29fb556
Yanglichao <18201695833@163.com>
2020-10-21 17:09:49 +0800

    MOD：edit

--- app/v1/transformer/user/IndexTransformer.php
+++ app/v1/transformer/user/IndexTransformer.php
@@ -8,6 +8,11 @@
 
 namespace app\v1\transformer\user;
 
+use app\common\enum\PrettyFemaleLevelEnum;
+use app\common\enum\PrettyMaleLevelEnum;
+use app\common\enum\UserIsPrettyEnum;
+use app\common\enum\UserSexEnum;
+use app\common\enum\UserSwitchEnum;
 use app\common\transformer\TransformerAbstract;
 
 class IndexTransformer extends TransformerAbstract
@@ -15,8 +20,35 @@ class IndexTransformer extends TransformerAbstract
 
     public function transformData(array $data): array
     {
+        $userInfo = $data["userInfo"] ?? [];
+        $user = $data["user"] ?? [];
+        $userSet = $data["userSet"] ?? [];
         return [
-            "score" => $data["score"] ?? "0",
+            "user" => [
+                'id' => $user["id"] ?? 0,
+                'sex' => $user['sex'] ?? UserSexEnum::UNKNOWN,
+                'age' => $this->getUserAge($userInfo['birthday'] ?? null),
+                'user_number' => $user['user_number'] ?? "",
+                'avatar' => $userInfo['portrait'] ?? "",
+                'nickname' => $userInfo['nickname'] ?? "",
+                'birthday' => $userInfo["birthday"] ?? "",
+                'photos' => empty($userInfo["photos"]) ? [] : json_decode($userInfo["photos"], true),
+                'city' => $userInfo['city'] ?? "",
+                'is_pretty' => $userInfo["is_pretty"] ?? UserIsPrettyEnum::NO,
+                'pretty_female_level' => $userInfo["pretty_female_level"] ?? PrettyFemaleLevelEnum::COMMON,
+                'pretty_male_level' => $userInfo['pretty_male_level'] ?? PrettyMaleLevelEnum::COMMON,
+                'member_level' => $userInfo['member_level'] ?? 0,
+                'is_vip' => !empty($userInfo['svip_level']) ? 1 : empty($userInfo['vip_level']) ? 0 : 1,
+                'is_svip' => empty($userInfo['svip_level']) ? 0 : 1,
+                'signatures' => empty($userInfo['signatures']) ? [] : json_decode($userInfo['signatures'],true),
+                'voice_chat_switch' => $userSet['voice_chat_switch'] ?? UserSwitchEnum::OFF,
+                'voice_chat_price' => $userSet['voice_chat_price'] ?? 0,
+                'video_chat_switch' => $userSet['video_chat_switch'] ?? UserSwitchEnum::OFF,
+                'video_chat_price' => $userSet['video_chat_price'] ?? 0,
+                'direct_message_free' => $userSet['direct_message_free'] ?? UserSwitchEnum::OFF,
+                'direct_message_price' => $userSet['direct_message_price'] ?? 0,
+                "score" => $data["score"] ?? "0",
+            ],
             "dynamics" => $this->getDynamic($data["dynamics"], $data["dynamicLike"]),// 动态
             "videos" => $this->getVideos($data['videos'] ?? []), // 视频
             "gifts" => $this->getGifts($data["gifts"] ?? []),
@@ -73,5 +105,16 @@ class IndexTransformer extends TransformerAbstract
             return [];
         }
         return $gifts;
+    }
+
+    /**
+     *  年龄
+     *
+     * @param $birthday
+     * @return false|int|string
+     */
+    private function getUserAge($birthday)
+    {
+        return isset($birthday) ? date('Y') - substr($birthday, 0, 4) : 0;
     }
 }

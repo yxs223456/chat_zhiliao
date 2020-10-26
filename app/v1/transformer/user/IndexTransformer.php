d9bed5363763ebeac1e52abeaca8522a09f168b7
Yanglichao <18201695833@163.com>
2020-10-26 17:36:11 +0800

    MOD:edit

--- app/v1/transformer/user/IndexTransformer.php
+++ app/v1/transformer/user/IndexTransformer.php
@@ -53,7 +53,7 @@ class IndexTransformer extends TransformerAbstract
             "dynamics" => $this->getDynamic($data["dynamics"], $data["dynamicLike"]),// 动态
             "videos" => $this->getVideos($data['videos'] ?? []), // 视频
             "gifts" => $this->getGifts($data["gifts"] ?? []),
-            "guard" => $this->getGuard($data["guard"] ?? []),
+            "angle" => $this->getGuard($data["guard"] ?? []),
         ];
     }
 

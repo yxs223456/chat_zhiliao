0f17fc24c2c433bd937b149236c02a3e082e20a6
yangxiushan <501942821@qq.com>
2020-11-04 10:25:55 +0800

    调试通话

--- app/common/service/ChatService.php
+++ app/common/service/ChatService.php
@@ -144,8 +144,12 @@ class ChatService extends Base
             throw $e;
         }
 
+        // 声网token
+        $swTokenInfo = $this->getSwRtcToken($user, $chatId);
+
         $returnData = [
             "chat_id" => $chatId,
+            "sw_token_info" => $swTokenInfo,
         ];
 
         return $returnData;
@@ -293,10 +297,14 @@ class ChatService extends Base
             throw $e;
         }
 
+        // 声网token
+        $swTokenInfo = $this->getSwRtcToken($user, $chatId);
+
         $returnData = [
             "is_free" => $isFree,
             "current_time" => time(),
             "deadline" => $isFree ? 0 : time() + ($minutes * 60),
+            "sw_token_info" => $swTokenInfo,
         ];
         return $returnData;
     }

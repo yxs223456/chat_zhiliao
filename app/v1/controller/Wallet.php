169d08188fc529015be702902bfc98bf09773a0b
yangxiushan <501942821@qq.com>
2020-11-11 11:11:58 +0800

    钱包 controller

---
+++ app/v1/controller/Wallet.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangxiushan
+ * Date: 2020-11-11
+ * Time: 11:06
+ */
+
+namespace app\v1\controller;
+
+use app\common\AppException;
+use app\common\service\WalletService;
+use app\v1\transformer\wallet\IndexTransformer;
+use app\v1\transformer\wallet\PayByAliTransformer;
+use app\v1\transformer\wallet\PayByWeChatTransformer;
+
+class Wallet extends Base
+{
+
+    protected $beforeActionList = [
+        "getUser" => [
+            "except" => "",
+        ],
+        "checkSex" => [
+            "except" => "",
+        ]
+    ];
+
+    /**
+     * 充值规格获取
+     *
+     * @return \think\response\Json
+     */
+    public function index()
+    {
+        $service = new WalletService();
+        $data = $service->index();
+        return $this->jsonResponse($data, new IndexTransformer());
+    }
+
+    /**
+     * 微信充值
+     *
+     * @return \think\response\Json
+     * @throws AppException
+     */
+    public function payByWeChat()
+    {
+        $request = $this->query["content"];
+        $rechargeId = $request["id"] ?? 0;
+        if (!checkInt($rechargeId, false)) {
+            throw AppException::factory(AppException::QUERY_PARAMS_ERROR);
+        }
+
+        $user = $this->query["user"];
+
+        $service = new WalletService();
+        $data = $service->payByWeChat($rechargeId, $user);
+        return $this->jsonResponse($data, new PayByWeChatTransformer());
+    }
+
+    /**
+     * 支付宝充值
+     *
+     * @return string
+     * @throws AppException
+     */
+    public function payByAli()
+    {
+        $request = $this->query["content"];
+        $rechargeId = $request["id"] ?? 0;
+        if (!checkInt($rechargeId, false)) {
+            throw AppException::factory(AppException::QUERY_PARAMS_ERROR);
+        }
+
+        $user = $this->query["user"];
+
+        $service = new WalletService();
+        $data = $service->payByAli($rechargeId, $user);
+        return $this->jsonResponse(["url" => $data], new PayByAliTransformer());
+    }
+}
---
+++ app/v1/transformer/wallet/IndexTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\v1\transformer\wallet;
+
+use app\common\transformer\TransformerAbstract;
+
+/**
+ * Created by PhpStorm.
+ * User: lichaoyang
+ * Date: 2020/9/29
+ * Time: 下午4:35
+ */
+class IndexTransformer extends TransformerAbstract
+{
+    public function transformData(array $data)
+    {
+        return [
+            'id' => $data['id'] ?? 0,
+            'coin_price' => (int)$data["coin_price"] ?? 0,
+            'price' => (int)$data["price"] ?? 0,
+            'is_new' => $data["is_new"] ?? 0,
+            'is_wechat' => $data["is_wechat"] ?? 0,
+            'is_alipay' => $data["is_alipay"] ?? 0,
+            'gaving' => (int)$data["gaving"] ?? 0
+        ];
+    }
+}

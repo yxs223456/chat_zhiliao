5f0f55f43a0f7ef309c6d45d46e02b9ca30be19e
yangxiushan <501942821@qq.com>
2020-11-13 16:08:21 +0800

    钱包 controller

--- app/v1/transformer/wallet/IndexTransformer.php
+++ app/v1/transformer/wallet/IndexTransformer.php
@@ -18,10 +18,6 @@ class IndexTransformer extends TransformerAbstract
             'id' => $data['id'] ?? 0,
             'coin_price' => (int)$data["coin_price"] ?? 0,
             'price' => (int)$data["price"] ?? 0,
-            'is_new' => $data["is_new"] ?? 0,
-            'is_wechat' => $data["is_wechat"] ?? 0,
-            'is_alipay' => $data["is_alipay"] ?? 0,
-            'gaving' => (int)$data["gaving"] ?? 0
         ];
     }
 }

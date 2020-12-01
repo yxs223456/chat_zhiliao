ddf5f020a9cdd46ae1fbf9372b8035f3ad1a001b
yangxiushan <501942821@qq.com>
2020-12-01 16:45:05 +0800

    检测用户在线

--- app/common/service/UserService.php
+++ app/common/service/UserService.php
@@ -822,7 +822,7 @@ class UserService extends Base
         // 关闭逻辑直接关闭
         // 开启关闭的修改数据库操作
         Db::name("user_set")->where("u_id", $user["id"])->update(["is_stealth" => $switch]);
-        deleteUserSetByUId($user["id"], Redis::factory());
+        deleteUserSetByUId($user["id"], $redis);
         if ($switch == UserSwitchEnum::ON) {
             if (!empty($userInfo["city"])) {
                 homeSetCacheCallbackProduce($user["id"], "popCity", $redis, $userInfo["city"]);
